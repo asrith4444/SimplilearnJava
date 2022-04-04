d209ae1bc3c608deb73af20393ee563cfc081479
Asrith Krishna <51793301+asrith4444@users.noreply.github.com>
2022-04-04 22:18:32 +0530

    Add files via upload

---
+++ src/module1/ArrayRotation.java
@@ -0,0 +1,30 @@
+package module1;
+
+class RotateArray { 
+    public void rotate(int[] nums, int k) {
+        if(k > nums.length) { // 3>7 
+            k=k%nums.length;
+        }
+        int[] result = new int[nums.length];//result array of size 7
+        for(int i=0; i < k; i++){
+            result[i] = nums[nums.length-k+i];
+        }
+        int j=0;
+        for(int i=k; i<nums.length; i++){
+            result[i] = nums[j];
+            j++;
+        }
+        System.arraycopy( result, 0, nums, 0, nums.length );
+    }
+} 
+public class ArrayRotation
+{
+    public static void main(String[] args) {
+        RotateArray r = new RotateArray();
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        r.rotate(arr, 2); 
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
